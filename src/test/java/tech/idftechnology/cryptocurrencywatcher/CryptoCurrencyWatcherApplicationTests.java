0f10a057e92630bfb481e203bde2bbf64e6737ff
PummelerDev <PummelerDev@users.noreply.github.com>
2023-05-12 21:25:43 +0300

    initial commit

---
+++ src/test/java/tech/idftechnology/cryptocurrencywatcher/CryptoCurrencyWatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.idftechnology.cryptocurrencywatcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoCurrencyWatcherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
